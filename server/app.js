503a5ecd5f314c2e6db638619300a05c3a22ff6a
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2022-11-06 03:12:27 +0100

    database connection

--- server/app.js
+++ server/app.js
@@ -1,6 +1,19 @@
-var express = require('express');
-var app = express();
-var port = 3000;
+const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true });
+const { connection } = mongoose;
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+});
+
 
 app.get('/', (req, res) => {
   res.send('hello world')
