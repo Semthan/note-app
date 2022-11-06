c0b83aa2fde733b3d0e7c68753aedad1d667e6c3
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2022-11-06 23:16:38 +0100

    created controller for note

--- server/models/noteModel.js
+++ server/models/noteModel.js
@@ -4,11 +4,10 @@ const Schema = moongose.Schema;
 
 const noteSchema = new Schema({
   title: { type: String, required: true },
-  content: { type: String, required: true },
+  content: { type: String, required: true }
 }, {
   timestamps: true,
 });
 
-const Note = moongose.model('Note', noteSchema);
+module.exports = moongose.model('Note', noteSchema);
 
-module.exports = Note;
