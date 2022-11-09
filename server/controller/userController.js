a1f4354d3388eb89e1f36c228e33c1a80a946ae2
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2022-11-09 23:25:55 +0100

    user log in

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -39,7 +39,19 @@ exports.registerUser = async (req, res) => {
 
 exports.loginUser = async (req, res) => {
   try {
-    res.status(200).json('login user')
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      res.json({
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+      })
+    } else {
+      res.status(400).json('invalid credentials');
+    }
   } catch (error) {
     console.error(error)
     res.status(500).send
