7251db6adad779cfe1f3a0ce0a64c79ff9f16602
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2022-11-09 23:09:12 +0100

    register new user

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,8 +1,36 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/userModel');
 
 exports.registerUser = async (req, res) => {
   try {
-    res.status(200).json('register')
+    const { name, email, password } = req.body;
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      res.status(400).json('user already exists');
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    if (user) {
+      res.status(201).json({
+        _id: user.id,
+        name: user.name,
+        email: user.email
+      })
+    }
+    else {
+      res.status(400).json('invalid user data');
+    }
   } catch (error) {
     console.error(error)
     res.status(500).send
