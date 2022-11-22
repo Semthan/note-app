cf8e96ad720407b8441c0d8a29dd3e5e26bc66ef
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2022-11-22 01:47:02 +0100

    protect note routes

--- server/controller/noteController.js
+++ server/controller/noteController.js
@@ -1,8 +1,9 @@
 const Note = require('../models/noteModel');
+const User = require('../models/userModel');
 
 exports.getAllNotes = async (req, res) => {
   try {
-    const allNotes = await Note.find({})
+    const allNotes = await Note.find({ user: req.user.id })
     res.status(200).json(allNotes)
   } catch (error) {
     console.error(error)
@@ -25,7 +26,8 @@ exports.addNote = async (req, res) => {
   try {
     const note = await Note.create({
       title: req.body.title,
-      content: req.body.content
+      content: req.body.content,
+      user: req.user.id,
     })
     res.status(200).json(note)
   } catch (error) {
@@ -36,6 +38,22 @@ exports.addNote = async (req, res) => {
 
 exports.editNote = async (req, res) => {
   try {
+    const note = await Note.findById(req.params.id)
+
+    if (!note) {
+      res.status(400).json('Note not found');
+    }
+
+    const user = await User.findById(req.user.id)
+
+    //check for user
+    if (!user) {
+      res.status(401).json('User not found');
+    }
+
+    if (note.user.toString() !== user.id) {
+      res.status(401).json('user not authorized');
+    }
     const updatedNote = await Note.findByIdAndUpdate(req.params.id, req.body, { new: true });
     res.status(200).json(updatedNote)
   } catch (error) {
@@ -46,9 +64,23 @@ exports.editNote = async (req, res) => {
 
 exports.deleteOneNote = async (req, res) => {
   try {
-    const noteID = req.params.id;
-    const deletedNote = await Note.findByIdAndDelete(noteID);
-    res.status(200).json(deletedNote);
+    const note = await Note.findById(req.params.id);
+
+    if (!note) {
+      res.status(400).json('Note not found');
+    }
+
+    if (!req.user) {
+      res.status(401).json('User not found');
+    }
+
+    if (note.user.toString() !== req.user.id) {
+      res.status(401).json('user not authorized');
+    } else {
+      await note.remove();
+    }
+
+    res.status(200).json({ id: req.params.id });
   } catch (error) {
     console.error(error)
     res.status(500).send()
--- server/routes/note.js
+++ server/routes/note.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const router = express.Router();
-
 const noteController = require('../controller/noteController');
+const { protect } = require('../middleware/authMiddleware');
+
+
+
 
 router
-  .get('/', noteController.getAllNotes)
-  .post('/', noteController.addNote)
+  .get('/', protect, noteController.getAllNotes)
+  .post('/', protect, noteController.addNote)
 
 router
-  .get('/:id', noteController.getOneNote)
-  .put('/:id', noteController.editNote)
-  .delete('/:id', noteController.deleteOneNote)
+  .get('/:id', protect, noteController.getOneNote)
+  .put('/:id', protect, noteController.editNote)
+  .delete('/:id', protect, noteController.deleteOneNote)
 
 module.exports = router;
