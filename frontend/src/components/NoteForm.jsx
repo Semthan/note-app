e7a0792d4dba62983b59c9bea205680a85cc8a13
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2022-11-29 02:36:47 +0100

    note async thunk

--- frontend/src/components/NoteForm.jsx
+++ frontend/src/components/NoteForm.jsx
@@ -1,27 +1,42 @@
 import {useState} from 'react'
 import {useDispatch} from 'react-redux'
-//import {createNote} from '../features/notes/noteSlice'
+import {createNote} from '../features/notes/noteSlice'
 
 function NoteForm() {
-  const [text,setText] = useState('')
+  const [title,setTitle] = useState('')
+  const [content,setContent] = useState('')
 
   const dispatch = useDispatch()
 
   const onSubmit = (e) => {
     e.preventDefault()
 
+    dispatch(createNote({title, content}))
+    setTitle('')
+    setContent('')
   }
+
   return (
     <section className='form'>
       <form onSubmit={onSubmit}>
         <div className="form-group">
-          <label htmlFor="text">Note</label>
+          <label htmlFor="title">Title</label>
           <input
-            type="text"
-            name='text'
-            id='text'
-            value={text}
-            onChange={(e) => setText(e.target.value)} 
+            type="title"
+            name='title'
+            id='title'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)} 
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="content">Content</label>
+          <input
+            type="content"
+            name='content'
+            id='content'
+            value={content}
+            onChange={(e) => setContent(e.target.value)} 
           />
         </div>
         <div className="form-group">
