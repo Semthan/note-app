6d7676ac232f8352e964e12d403e29ff43c07958
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2022-12-13 16:35:53 +0100

    deploy test_2

--- backend/app.js
+++ backend/app.js
@@ -27,10 +27,9 @@ app.use('/note', noteRouter);
 app.use('/user', userRouter);
 
 
-const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+  res.sendFile(path.resolve(__dirname, '/frontend/build/index.html'))
 );
 
 app.listen(port, () => {
